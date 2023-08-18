dc78689d5cce5d5636eaa5b29936cb2c0aeacc2f
samyosm <samy.osmium@gmail.com>
2023-08-18 14:21:51 -0400

    init: admin

---
+++ apps/admin/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Index() {
+  return (
+    <div className="min-h-screen bg-indigo-500 text-white font-bold text-3xl flex items-center justify-center">
+      <p>Hello, world!</p>
+    </div>
+  );
+}
